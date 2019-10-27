2a75f24e6cffd8bffc38c7242410c44adc7e24da
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2019-10-27 08:22:30 -0700

    added build directory to repo

---
+++ build/precache-manifest.1bd419aede60c9a5c723da92d8438a78.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9abbdf475a01697a2509090ee3df0d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "22eb7ee831d43c9c34de",
+    "url": "./static/css/main.f29bd2cf.chunk.css"
+  },
+  {
+    "revision": "3c3d880545678aa4d37f",
+    "url": "./static/js/2.982000c3.chunk.js"
+  },
+  {
+    "revision": "22eb7ee831d43c9c34de",
+    "url": "./static/js/main.8c97abab.chunk.js"
+  },
+  {
+    "revision": "d4df5b79ff44564398bb",
+    "url": "./static/js/runtime-main.1ddcbf1f.js"
+  },
+  {
+    "revision": "26d5b36bd369ca091d17c00f59307666",
+    "url": "./static/media/0.26d5b36b.png"
+  },
+  {
+    "revision": "24de9c673d9962351d8f41600dfa84e0",
+    "url": "./static/media/1.24de9c67.png"
+  },
+  {
+    "revision": "d5f2d652a5185009d72a6ed69aca633f",
+    "url": "./static/media/2.d5f2d652.png"
+  },
+  {
+    "revision": "f4b8692845c0203880ff90b4149827ba",
+    "url": "./static/media/3.f4b86928.png"
+  },
+  {
+    "revision": "ca8bf4132c1df8b873760638d91fd847",
+    "url": "./static/media/4.ca8bf413.png"
+  },
+  {
+    "revision": "431c58e9817876bbdf69cbb5b972bb1b",
+    "url": "./static/media/5.431c58e9.png"
+  },
+  {
+    "revision": "076256681ce9bb4c07cee15a238ca379",
+    "url": "./static/media/6.07625668.png"
+  },
+  {
+    "revision": "169f4309d2b6b20c9557aa12cf353d3b",
+    "url": "./static/media/7.169f4309.png"
+  }
+]);
